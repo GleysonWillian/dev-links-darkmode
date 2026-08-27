186c47ef3cbf176375932a3723d88251b4a15df6
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2026-08-26 22:06:22 -0300

    refactor: improve formatting and consistency in user routes

--- sistema-cadastro/server.js
+++ sistema-cadastro/server.js
@@ -16,11 +16,11 @@ app.get("/usuarios", (req, res) => {
 app.get("/usuarios/:id", (req, res) => {
   const id = Number(req.params.id)
 
-  const usuario =  usuarios.find(usuario => usuario.id === id)
+  const usuario = usuarios.find((usuario) => usuario.id === id)
 
-  if(!usuario) {
+  if (!usuario) {
     return res.status(404).json({
-      mensagem: "Usuário não encontrado"
+      mensagem: "Usuário não encontrado",
     })
   }
 
@@ -43,6 +43,44 @@ app.post("/usuarios", (req, res) => {
   return res.status(201).json({
     mensagem: "Usuário criado com sucesso!",
     usuario: usuario,
+  })
+})
+
+app.put("/usuarios/:id", (req, res) => {
+  const id = Number(req.params.id)
+
+  const usuario = usuarios.find((usuario) => usuario.id === id)
+
+  if (!usuario) {
+    return res.status(404).json({
+      mensagem: "Usuário não encontrado!",
+    })
+  }
+
+  usuario.nome = req.body.nome
+  usuario.email = req.body.email
+
+  return res.json({
+    mensagem: "Usuário atualizado com sucesso!",
+    usuario: usuario,
+  })
+})
+
+app.delete("/usuarios/:id", (req, res) => {
+
+  const id = Number(req.params.id)
+
+  const indice = usuarios.findIndex(usuario => usuario.id === id)
+
+  if (indice === -1) {
+    return res.status(404).json({
+      mensagem: "Usuário não encontrado!",
+    })
+  }
+  usuarios.splice(indice, 1)
+
+  return res.json({
+    mensagem: "Usuário removido com sucesso!",
   })
 })
 
